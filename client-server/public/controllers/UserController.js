a000aa787ea04502b967dbe8b11bfdea8062ba4b
Diogo Lima <diogoslva013@gmail.com>
2022-10-03 17:38:32 -0300

    Using Ajax with POST and PUT method

--- client-server/public/controllers/UserController.js
+++ client-server/public/controllers/UserController.js
@@ -51,7 +51,7 @@ class UserController {
 
                     user.loadFromJSON(result);
 
-                    user.save();
+                    user.save().then(user=>{
 
                     this.getTr(user, tr);
 
@@ -62,6 +62,9 @@ class UserController {
                     btn.disabled = false;
 
                     this.showPanelCreate();
+
+
+                    });
 
                 },
                 (e) => {
@@ -92,13 +95,16 @@ class UserController {
                     
                     values.photo = content;
 
-                    values.save();
+                    values.save().then(user=>{
 
-                    this.addLine(values);
+                    this.addLine(user);
 
                     this.formEl.reset();
 
                     btn.disabled = false;
+
+                    });
+
 
                 }, 
                 (e) => {
