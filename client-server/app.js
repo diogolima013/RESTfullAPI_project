1d80490ee4789930dc4fef846ea3943bf62a2e59
Diogo Lima <diogoslva013@gmail.com>
2022-10-04 08:46:21 -0300

    finishing the project

--- client-server/app.js
+++ client-server/app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' })); //limite img
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
